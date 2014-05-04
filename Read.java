7be3c4b3f67cfaccd26045292f621c99e8ba2cc6
Eduardo Ogliari <egliari@gmail.com>
2014-05-03 21:22:08 -0300

    Le e armazena conteudo do Scanner em uma array de String storeWords, e logo em seguida imprime a mesma.

--- Read.java
+++ Read.java
@@ -5,13 +5,25 @@ import java.io.File;
 public class Read{
 
 	public static void main(String args[]){
-	
+		int j = 0;
+		int i = 0;
+		String storeWords[] = new String[2000];
+		
 		try{
 			File fileToRead = new File(args[0]);
 			Scanner s = new Scanner(fileToRead);
 			
-			while(s.hasNextLine()){	
-				System.out.println(s.nextLine());
+			while(s.hasNextLine()){
+				storeWords[i] = s.nextLine();
+				//System.out.println(s.nextLine());
+				i++;
+			}
+			
+			
+			System.out.println("A PARTIR DAQUI O QUE ESTA GUARDADO NO STOREWORDS");
+			while(storeWords[j] != null ){
+				System.out.println(storeWords[j]);
+				j++;
 			}
 			
 		}catch(Exception e){
