53c544022ea2de94fa3821760d3bcf917f824228
Eduardo Ogliari <egliari@gmail.com>
2014-05-11 15:08:35 -0300

    Read.java    Pega um arquivo da linha de comando e utiliza String.split() para separar todas a strings quando encontrar espaços em branco e armazena em uma array de string. Logo após, o valor de todos os índices são copiados para um novo array exceto nos quais são encontrados espaços em branco. A classe Interpretador é instanciada no objeto de classe "interpret", cujo métodos são chamados para iniciar o processo de interpretação.
    Interpretador.java
       Declarados Maps para quando for detectado declaração de variáveis no código.

---
+++ Interpretador.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Interpretador {
+	
+	Map <String, String> stringVariables = new HashMap<String, String>();
+	Map <String, Integer> integerVariables = new HashMap<String, Integer>();
+	Map <String, Float> floatVariables = new HashMap<String, Float>();
+	Map <String, Double> doubleVariables = new HashMap<String, Double>();
+	
+	String [] tokens = new String[2000];
+	
+	
+	public void setTokens(String storeTokens[]){
+		this.tokens = storeTokens;
+	}
+	
+	
+	public void iniciaInterpretacao(){
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+
+
+}
--- Read.java
+++ Read.java
@@ -1,30 +1,57 @@
-import java.util.Scanner;
-import java.io.BufferedReader;
+import java.util.Scanner;  
 import java.io.File;
 
 public class Read{
 
 	public static void main(String args[]){
-		int j = 0;
+		
+		Interpretador interpret = new Interpretador();	
 		int i = 0;
 		String storeWords[] = new String[2000];
-		
+		String storeTokens[] = new String[3000];
+				
 		try{
+			
 			File fileToRead = new File(args[0]);
 			Scanner s = new Scanner(fileToRead);
 			
 			while(s.hasNextLine()){
 				storeWords[i] = s.nextLine();
-				//System.out.println(s.nextLine());
 				i++;
 			}
 			
+			int f = 0;
+			String temp[] = new String[200];
 			
-			System.out.println("A PARTIR DAQUI O QUE ESTA GUARDADO NO STOREWORDS");
-			while(storeWords[j] != null ){
-				System.out.println(storeWords[j]);
-				j++;
+			
+			while(storeWords[f] != null){
+				int g = 0;
+				temp = storeWords[f].split("\\s");
+				
+				while(storeTokens[g] != null){
+					g++;
+				}
+					
+							
+				for(int y = 0; y < temp.length; y++){
+					if(temp[y].contentEquals("") == false){
+						storeTokens[g] = temp[y];
+						g++;
+					}
+				}
+				
+				temp = new String[temp.length];
+				f++;
+				
 			}
+			
+			// Comentado loop que imprime valores dentro de StoreTokens
+			/*for(int h = 0; storeTokens[h] != null; h++){
+				System.out.println(storeTokens[h]);
+			}*/
+			
+			interpret.setTokens(storeTokens);
+			interpret.iniciaInterpretacao();
 			
 		}catch(Exception e){
 			
